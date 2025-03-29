e2c1989233fde9b60c1014d2c311cf7bc6c7f89e
asifali-7890 <78083087+asifali-7890@users.noreply.github.com>
2025-03-29 18:33:08 +0530

    Add build script to package.json and serve static files from 'dist' directory in server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import connectDB from './config/db.js';
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Load environment variables
 dotenv.config();
@@ -19,7 +21,15 @@ connectDB();
 
 // Create an Express app
 const app = express();
+
+// Get the directory name for ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
+
 // Middleware to handle form data and file uploads
 
 // Middleware
@@ -36,6 +46,11 @@ app.use("/api/v1/contact", contactRoutes);
 // Define a basic route
 app.get('/', (req, res) => {
     res.send('Server is running on port 5000!');
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 // Start the server
